42189d0501c77f7bb343615aa0e0667d00a53abe
Katya <ekaterina.leo.nikiforova@gmail.com>
2021-03-27 18:53:12 +0300

    tying data from parent to child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    { title: 'Neat Tree', imageUrl: 'assets/tree.jpeg', username: 'nature', content: 'I saw this neat tree today' },
+    { title: 'Snowy Mountain', imageUrl: 'assets/mountain.jpeg', username: 'mountainlovaer', content: 'Here is a pic of a snowy mountain' },
+    { title: 'Mountain Biking', imageUrl: 'assets/biking.jpeg', username: 'biking222', content: 'I did some biking today' }
+  ]
 }
